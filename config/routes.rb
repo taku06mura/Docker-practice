a5bffb691fb58bc67f870771a3bbb6484d306089
taku06mura <t.lux.g1106@icloud.com>
2020-04-09 03:02:19 +0900

    create boards-show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "boards#index"
-  resources :boards, only: [:index, :new, :create]
+  resources :boards, only: [:index, :new, :create, :show]
   # get 'boards', to: 'boards#index'
   # get 'boards/new', to: 'boards#new'
   # post 'boards', to: 'boards#create'
