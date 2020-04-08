7cf213d5a4070b1d0ed73deb9973c4b0bf2b923e
taku06mura <t.lux.g1106@icloud.com>
2020-04-09 01:05:23 +0900

    add index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
+  root "boards#index"
+  resources :boards, only: [:index, :new, :create]
+  # get 'boards', to: 'boards#index'
+  # get 'boards/new', to: 'boards#new'
+  # post 'boards', to: 'boards#create'
 end
